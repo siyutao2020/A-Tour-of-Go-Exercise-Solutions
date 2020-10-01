2ba57110b18b7c1fccb0f7236bd275156f6c7226
Siyu Tao <siyutao2020@u.northwestern.edu>
2020-10-01 17:41:01 -0400

    Update Loops_and_Functions_sol.go

--- Loops_and_Functions_sol.go
+++ Loops_and_Functions_sol.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 )
 
+const thres = 1e-16
+
 func Sqrt(x float64) float64 {
 	z := 1.0
-	for (z*z-x)*(z*z-x) > 1e-16 {
+	for (z*z-x)*(z*z-x) > thres {
 		z -= (z*z - x) / (2 * z)
 	}
 	return z
