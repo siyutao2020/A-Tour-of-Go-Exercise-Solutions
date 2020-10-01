63485043cd7fc37c4c8b646b6cc94e1f28216c99
Siyu Tao <siyutao2020@u.northwestern.edu>
2020-10-01 17:48:32 -0400

    Create Stringers2_sol.go
    Another solution to the Stringers exercise.

---
+++ Stringers2_sol.go
@@ -0,0 +1,27 @@
+package main
+
+import ("fmt"
+		"strconv"
+		"strings"
+	   )
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr
+func (ip IPAddr) String() string {
+	var strs []string
+	for _, b := range(ip) {
+		strs = append(strs, strconv.Itoa(int(b)))
+	}
+	return strings.Join(strs, ".")
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
