ede4e5abe653bfc484c8979b8d9cac014f9a8970
Siyu Tao <siyutao2020@u.northwestern.edu>
2020-10-01 17:35:00 -0400

    Create Readers_sol.go
    Solution to the Readers exercise.

---
+++ Readers_sol.go
@@ -0,0 +1,17 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (myReader MyReader) Read(bytes []byte) (int, error) {
+	for i := range bytes {
+		bytes[i] = 'A'
+	}
+	return len(bytes), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
